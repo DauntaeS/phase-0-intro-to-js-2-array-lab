6b91b786b954d57488c73857b1bd71a47effa73e
Dauntae Stewart <dauntae9@gmail.com>
2022-03-20 00:54:02 -0500

    commit tree 96faab9e61937f0ba737aa79ee97e905f73563c8

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph);
+}
+function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+}
+function destructivelyRemoveLastCat(Bob) {
+    cats.pop(3);
+}
+function destructivelyRemoveFirstCat(Ralph) {
+    cats.shift(1);
+}
+function appendCat(Broom) {
+    const catLove = [...cats, Broom];
+    return catLove;
+}
+function prependCat(Arnold) {
+    const catLover = [Arnold, ...cats];
+    return catLover;
+}
+function removeLastCat(Bob) {
+    const meow = cats.slice(0, 2);
+    return meow;
+}
+function removeFirstCat(Dog) {
+    const tree = cats.slice(1);
+    return tree; 
+}
